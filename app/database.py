b9933721f5c6734e339e3af39c8f1e123a844cc9
Tonylu20 <slssalad@gmail.com>
2025-05-12 22:08:30 -0600

    BACKUPS POR USUARIOS

--- app/database.py
+++ app/database.py
@@ -1,5 +1,6 @@
 from app.extensions import db, login_manager
 from flask_login import UserMixin
+from datetime import datetime
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -9,3 +10,12 @@ class User(UserMixin, db.Model):
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
+
+class Backup(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    filename = db.Column(db.String(255), nullable=False)
+    size_kb = db.Column(db.Float, nullable=False)
+    timestamp = db.Column(db.DateTime, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    user = db.relationship('User', backref='backups')
